6a53867cec006a05f256c2678def4c41d9086abd
Robinson de la Hoz <rahoz@indracompany.com>
2026-07-28 17:15:29 -0500

    session: week-01 skeleton TrackingIdGenerator + pom.xml

---
+++ sessions/week-01-kickoff/src/main/java/com/indra/logistics/TrackingIdGenerator.java
@@ -0,0 +1,15 @@
+package com.indra.logistics;
+
+public class TrackingIdGenerator {
+
+    /**
+     * Genera un ID de seguimiento con formato ORIG-DEST-XXXXXXXX
+     * @param origin  código de origen (ej: "BOG")
+     * @param destination código de destino (ej: "MED")
+     * @return ID único de seguimiento
+     */
+    public String generate(String origin, String destination) {
+        // TODO: implementar
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
---
+++ sessions/week-01-kickoff/src/test/java/com/indra/logistics/TrackingIdGeneratorTest.java
@@ -0,0 +1,24 @@
+package com.indra.logistics;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrackingIdGeneratorTest {
+
+    private final TrackingIdGenerator generator = new TrackingIdGenerator();
+
+    @Test
+    @DisplayName("El ID generado debe tener el formato ORIG-DEST-XXXXXXXX")
+    void shouldGenerateIdWithCorrectFormat() {
+        // TODO: implementar
+        fail("Test no implementado");
+    }
+
+    @Test
+    @DisplayName("Debe lanzar excepción si origin es nulo")
+    void shouldThrowWhenOriginIsNull() {
+        // TODO: implementar
+        fail("Test no implementado");
+    }
+}
